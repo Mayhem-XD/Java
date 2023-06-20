9ef0135137e298771ecf77fb1fd98e553d39b40a
Mayhem-XD <leahblo1994@gmail.com>
2023-06-20 17:04:59 +0900

    first commit

---
+++ src/ch01/HelloWorld.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World!!!\n");
+		System.out.println("안녕하세요?\n");
+		System.out.println("123\n");
+		
+	}
+
+}
