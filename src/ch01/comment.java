6a2bb74eaef47f91babba8a58c0c7fe5d4749196
Mayhem-XD <leahblo1994@gmail.com>
2023-06-21 10:01:20 +0900

    comment

---
+++ src/ch01/comment.java
@@ -0,0 +1,24 @@
+package ch01;
+
+public class comment {
+	/**
+	 * document comment
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// In-Line comment
+		/* comment */
+		
+		/*
+		 *	multi-line comment
+		 */
+		//	프로그램 시작 위치
+		System.out.println("실행문 뒤에는 세미콜론을 붙어야 함.");
+		int x = 1;
+		int y = 2;
+		int sum=x+y;
+		System.out.println("결과는 "+ sum + " 입니다.");
+		
+	}
+
+}
