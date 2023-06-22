6e4f3afcae2245f745d3d32a8940529ebe6c2803
Mayhem-XD <leahblo1994@gmail.com>
2023-06-22 09:32:31 +0900

    수정

--- src/ch04/Ex00_test02.java
+++ src/ch04/Ex00_test02.java
@@ -9,22 +9,30 @@ import java.util.Scanner;
 public class Ex00_test02 {
 
 	public static void main(String[] args) {
-		int bYear,bMonth,bDay,age,month,day;
+		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("생년> ");
-		bYear = scan.nextInt();
-		System.out.println("생월> ");
-		bMonth = scan.nextInt();
-		System.out.println("생일> ");
-		bDay = scan.nextInt();
+		
+		/*
+		 * int bYear,bMonth,bDay,age,month,day; System.out.println("생년> "); bYear =
+		 * scan.nextInt(); System.out.println("생월> "); bMonth = scan.nextInt();
+		 * System.out.println("생일> "); bDay = scan.nextInt();
+		 */
+		System.out.println("생년월일(yyyy-mm-dd)> ");
+		String[] birth = scan.nextLine().split("-");		
+		int bYear,bMonth,bDay,age,month,day;
 		scan.close();
+		bYear = Integer.parseInt(birth[0]);
+		bMonth = Integer.parseInt(birth[1]);
+		bDay = Integer.parseInt(birth[2]);
+		
+		
 		LocalDate today = LocalDate.now();
 //		System.out.printf("%d-%02d-%02d\n",today.getYear(),today.getMonthValue(),today.getDayOfMonth());
 		age = today.getYear() - bYear;
-		month = today.getMonthValue() - bMonth;
+		month = bMonth - today.getMonthValue();
 		day = today.getDayOfMonth() - bDay;
-		if (month >= 0 && day > 0) {
-			age--;
+		if (month > 0 || (month == 0 && day >= 0)) {
+		    age--;
 		}
 		System.out.println("만 "+age+" 세");
 		
