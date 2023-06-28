579554551ae47934975d6339957f0f1f06e77cb7
Mayhem-XD <leahblo1994@gmail.com>
2023-06-28 12:48:30 +0900

    추가

--- src/ch11/sec7/Ex02_LocalDateTime.java
+++ src/ch11/sec7/Ex02_LocalDateTime.java
@@ -16,6 +16,15 @@ public class Ex02_LocalDateTime {
 		LocalTime currTime = LocalTime.now();
 		System.out.println(currTime.toString().substring(0,8));
 
+		int year = today.getYear();
+		int month = today.getMonthValue();
+		int day = today.getDayOfMonth();
+		System.out.printf("%d-%02d-%02d\n",year,month,day);
+		
+		int hour = now.getHour();
+		int minute = now.getMinute();
+		int second = now.getSecond();
+		System.out.printf("%02d-%02d-%02d\n",hour,minute,second);
 	}
 
 }
