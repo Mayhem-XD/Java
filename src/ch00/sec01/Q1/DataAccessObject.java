fafaf36299d7bfd5b8e09383cfb5f627dc54578c
Joo <116787370+Mayhem-XD@users.noreply.github.com>
2023-07-13 21:11:40 +0900

    Create DataAccessObject.java

---
+++ src/ch00/sec01/Q1/DataAccessObject.java
@@ -0,0 +1,8 @@
+package ch00.sec01;
+
+public interface DataAccessObject {
+	public abstract void select();
+	public abstract void insert();
+	public abstract void update();
+	public abstract void delete();
+}
