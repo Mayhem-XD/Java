b5f846064b120c917b57c287c0b87dc936ee6177
Joo <116787370+Mayhem-XD@users.noreply.github.com>
2023-07-13 21:34:14 +0900

    Create Main.java

---
+++ src/ch00/sec01/Q2/Main.java
@@ -0,0 +1,19 @@
+package ch00.sec01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Set<Student> hashSet = new HashSet<>();
+		hashSet.add(new Student("1", "가가가"));
+		hashSet.add(new Student("2", "나나나"));
+		hashSet.add(new Student("1", "다다다")); 
+		
+		System.out.println(hashSet.toString());
+
+
+	}
+
+}
